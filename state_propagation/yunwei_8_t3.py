215daa81b5aad978edab2888fbc4217acf4fdf61
yunweilu <yunweilu2020@u.northwestern.edu>
2022-02-17 15:27:50 -0600

    Push

---
+++ state_propagation/yunwei_8_t3.py
@@ -0,0 +1,205 @@
+import numpy as np
+from matplotlib.lines import Line2D
+from scipy.sparse.linalg import eigs,norm
+from scipy.special import factorial
+import matplotlib.pyplot as plt
+from fractions import Fraction
+from scipy.sparse import csr_matrix,isspmatrix,bmat
+import scipy as sci
+def get_creation_operator(size,tp):
+    return np.diag(np.sqrt(np.arange(1, size),dtype=tp), k=-1)
+def get_annihilation_operator(size,tp):
+    return np.diag(np.sqrt(np.arange(1, size),dtype=tp), k=1)
+def get_s(A,b,tol):
+    s=1
+    if A.dtype==np.complex256:
+        s=np.ceil(_exact_1_norm(A))
+    else:
+        while(1):
+            norm_A = _exact_inf_norm(A)/s
+            max_term_notation=np.floor(norm_A)
+            max_term=1
+            for i in range(1,np.int(max_term_notation)):
+                max_term=max_term*norm_A/i
+                if max_term >= 10**16:
+                    break
+            if 10**-16 * max_term <= tol:
+                break
+            s=s+1
+    return s
+def _exact_1_norm(A):
+    # A compatibility function which should eventually disappear.
+    if sci.sparse.isspmatrix(A):
+        return max(abs(A).sum(axis=0).flat)
+    else:
+        return np.linalg.norm(A, 1)
+def _expm_multiply_simple_core_global(A, B, tol=None):
+    """
+    A helper function.
+    """
+    if tol is None:
+        tol =1e-16
+    s=get_s(A,B,tol)
+    F = B
+
+    c1 = _exact_1_norm(B)
+    j=0
+    while(1):
+        coeff = s*(j+1)
+        B =  A.dot(B)/coeff
+        c2 = _exact_1_norm(B)
+        F = F + B
+        if (c1+c2)<tol:
+            m=j+1
+            break
+        c1 = c2
+        j=j+1
+    F =  F
+    B = F
+    for i in range(1,int(s)):
+        c1=c2
+        for j in range(m):
+            coeff = s*(j+1)
+            B =  A.dot(B)/coeff
+            F = F + B
+        F =  F
+        B = F
+    return F, m*s
+def overnorm(A):
+    if A.dtype==np.complex256:
+        return _exact_inf_norm(A)
+    else:
+        return norm_two(A)
+def _exact_inf_norm(A):
+    # A compatibility function which should eventually disappear.
+    if sci.sparse.isspmatrix(A):
+        return max(abs(A).sum(axis=1).flat)
+    else:
+        return np.linalg.norm(A, np.inf)
+def norm_two(A):
+    if sci.sparse.isspmatrix(A):
+        A=csr_matrix(A).conjugate().transpose()
+        return np.sqrt(abs(eigs(A=A.dot(A),k=1,which='LM',return_eigenvectors=False)[0]))
+    else:
+        return np.linalg.norm(A)
+def norm_state(A):
+    return np.linalg.norm(A)
+def block_fre(A,E):
+    if isspmatrix(A) is False:
+        A = np.block([[A, E], [np.zeros_like(A), A]])
+    else:
+        A = bmat([[A, E], [None, A]]).tocsc()
+    return A
+def difference(A,B):
+    return overnorm(A-B)/overnorm(A)
+def get_H(dim,tp):
+    HILBERT_SIZE=dim
+    Q_dim=6
+    g=0.1*2*np.pi
+    anharmonicity =-0.225
+    a_dag = get_creation_operator(HILBERT_SIZE,tp)
+    a = get_annihilation_operator(HILBERT_SIZE,tp)
+    b_dag=get_creation_operator(Q_dim,tp)
+    b=get_annihilation_operator(Q_dim,tp)
+    A=np.kron(a,np.identity(Q_dim))
+    A_dag=np.kron(a_dag,np.identity(Q_dim))
+    B=np.kron(np.identity(HILBERT_SIZE),b)
+    B_dag=np.kron(np.identity(HILBERT_SIZE),b_dag)
+    H_trans = 1 / 2 * anharmonicity * np.dot(np.dot(b_dag, b),np.dot(b_dag,b)-np.identity(Q_dim))
+    H0=g*(np.kron(a_dag,b)+np.kron(a,b_dag))+np.kron(np.identity(HILBERT_SIZE),H_trans)
+    H=csr_matrix(-1j*(H0+0.5*2*np.pi*(np.dot(B_dag,B)+B+B_dag+1j*(B-B_dag))))
+    vec=1/np.sqrt(HILBERT_SIZE*Q_dim)*np.ones(HILBERT_SIZE*Q_dim)
+    return H,vec
+def get_auxiliary(dim,tp,t):
+    HILBERT_SIZE=dim
+    Q_dim=6
+    g=0.1*2*np.pi
+    anharmonicity =-0.225
+    a_dag = get_creation_operator(HILBERT_SIZE,tp)
+    a = get_annihilation_operator(HILBERT_SIZE,tp)
+    b_dag=get_creation_operator(Q_dim,tp)
+    b=get_annihilation_operator(Q_dim,tp)
+    A=np.kron(a,np.identity(Q_dim))
+    A_dag=np.kron(a_dag,np.identity(Q_dim))
+    B=np.kron(np.identity(HILBERT_SIZE),b)
+    B_dag=np.kron(np.identity(HILBERT_SIZE),b_dag)
+    H_trans = 1 / 2 * anharmonicity * np.dot(np.dot(b_dag, b),np.dot(b_dag,b)-np.identity(Q_dim))
+    H0=g*(np.kron(a_dag,b)+np.kron(a,b_dag))+np.kron(np.identity(HILBERT_SIZE),H_trans)
+    H=csr_matrix(-1j*(H0+0.5*2*np.pi*(np.dot(B_dag,B)+B+B_dag+1j*(B-B_dag))))
+    vec=1/np.sqrt(HILBERT_SIZE*Q_dim)*np.ones(2*HILBERT_SIZE*Q_dim)
+    vec[0:HILBERT_SIZE*Q_dim]=0
+    A=block_fre(t*H,-1j*(B+B_dag))
+    return A,vec
+def get_norm(ts):
+    tp=np.float64()
+    anorm=[]
+    for i in range(len(ts)):
+        HILBERT_SIZE=20
+        Q_dim=6
+        g=0.1*2*np.pi
+        anharmonicity =-0.225
+        a_dag = get_creation_operator(HILBERT_SIZE,tp)
+        a = get_annihilation_operator(HILBERT_SIZE,tp)
+        b_dag=get_creation_operator(Q_dim,tp)
+        b=get_annihilation_operator(Q_dim,tp)
+        A=np.kron(a,np.identity(Q_dim))
+        A_dag=np.kron(a_dag,np.identity(Q_dim))
+        B=np.kron(np.identity(HILBERT_SIZE),b)
+        B_dag=np.kron(np.identity(HILBERT_SIZE),b_dag)
+        H_trans = 1 / 2 * anharmonicity * np.dot(np.dot(b_dag, b),np.dot(b_dag,b)-np.identity(Q_dim))
+        H0=g*(np.kron(a_dag,b)+np.kron(a,b_dag))+np.kron(np.identity(HILBERT_SIZE),H_trans)
+        H=ts[i]*csr_matrix(-1j*(H0+0.5*2*np.pi*(np.dot(B_dag,B)+B+B_dag+1j*(B-B_dag))))
+        anorm.append(norm_two(H))
+    return anorm
+def get_norm_der(ts):
+    tp=np.float64()
+    anorm=[]
+    for i in range(len(ts)):
+        HILBERT_SIZE=20
+        Q_dim=6
+        g=0.1*2*np.pi
+        anharmonicity =-0.225
+        a_dag = get_creation_operator(HILBERT_SIZE,tp)
+        a = get_annihilation_operator(HILBERT_SIZE,tp)
+        b_dag=get_creation_operator(Q_dim,tp)
+        b=get_annihilation_operator(Q_dim,tp)
+        A=np.kron(a,np.identity(Q_dim))
+        A_dag=np.kron(a_dag,np.identity(Q_dim))
+        B=np.kron(np.identity(HILBERT_SIZE),b)
+        B_dag=np.kron(np.identity(HILBERT_SIZE),b_dag)
+        H_trans = 1 / 2 * anharmonicity * np.dot(np.dot(b_dag, b),np.dot(b_dag,b)-np.identity(Q_dim))
+        H0=g*(np.kron(a_dag,b)+np.kron(a,b_dag))+np.kron(np.identity(HILBERT_SIZE),H_trans)
+        H=csr_matrix(-1j*(H0+0.5*2*np.pi*(np.dot(B_dag,B)+B+B_dag+1j*(B-B_dag))))
+        A=ts[i]*block_fre(H,-1j*(A+A_dag))
+        anorm.append(norm_two(A))
+    return anorm
+theta_m=np.array([[8.70950435e-01, 2.48853043e+00, 4.02062662e+00, 5.51075280e+00,
+        6.97775212e+00, 8.43027209e+00, 9.87290220e+00, 1.13089234e+01,
+        1.27393366e+01, 1.41653119e+01, 1.55887688e+01],
+       [1.79283319e-01, 1.30547755e+00, 2.68990434e+00, 4.11674468e+00,
+        5.55078449e+00, 6.98409736e+00, 8.41474411e+00, 9.84240944e+00,
+        1.12672525e+01, 1.26895571e+01, 1.41096178e+01],
+       [0.13,1, 2.2, 3.6,
+        4.9, 6.3, 7.7, 9.1,
+        11, 12, 1.3],
+       [5.87845778e-03, 3.11153531e-01, 1.11496247e+00, 2.19235380e+00,
+        3.40348589e+00, 4.68492721e+00, 6.00642014e+00, 7.35228229e+00,
+        8.71376145e+00, 1.00856663e+01, 1.14647607e+01],
+        [0.003307471010225917,0.24259255239783006,0.9560279579613657,1.9645322216064995,3.1267463661459876,4.372820117778894,5.667919829026178,6.993478543263042,8.338922480962493,9.697877908033124,11.066316441049558 ],
+       [2.3e-03, 0.14, 0.64, 1.4,
+        2.4, 3.5, 4.7, 6,
+        7.2, 8.5, 9.9]])
+
+
+theta_mm=[]
+for j,_theta in enumerate(theta_m):
+    a={5:0,10:0,15:0,20:0,25:0,30:0,35:0,40:0,45:0,50:0,55:0}
+    for i, theta in enumerate(_theta):
+        a[5*(i+1)] = theta
+    theta_mm.append(a)
+tol = 2**-24
+dim=20
+t=3
+H,vec=get_H(dim,np.float64)
+for i in range(10000):
+    a,x1=_expm_multiply_simple_core_global(t*H, vec, tol=tol)
