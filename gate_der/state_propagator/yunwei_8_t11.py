710bdcd8e13c9e55757fb4f010d6a4c13cf1fe29
yunweilu <yunweilu2020@u.northwestern.edu>
2022-07-24 16:28:23 -0500

    test

--- gate_der/state_propagator/yunwei_8_t11.py
+++ gate_der/state_propagator/yunwei_8_t11.py
@@ -18,7 +18,6 @@ def _exact_1_norm(A):
         return max(abs(A).sum(axis=0).flat)
     else:
         return np.linalg.norm(A, 1)
-
 def gamma_fa(n):
     u = 1.11e-16
     return n*u/(1-n*u)
@@ -55,11 +54,14 @@ def residue_norm(m,norm_B,term):
         if term<1e-15:
             break
     return R_m
+
 def choose_ms(norm_A,d,tol):
     no_solution=True
     for i in range(1,int(np.floor(norm_A))):
+        if no_solution == False:
+            break
         norm_B = norm_A / i
-        l=int(np.ceil(norm_B))
+        l=int(np.floor(norm_B))
         beta_factor,last_term=beta(norm_B,l,d)
         lower_bound = i*(beta_factor)
         if lower_bound>tol:
@@ -68,28 +70,33 @@ def choose_ms(norm_A,d,tol):
         m_pass_lowbound=False
         for j in range(1,100):
             if j>l:
-                beta_factor=beta_factor+gamma_fa(j*(d+2)+2)*last_term*norm_B/j
+                last_term=last_term*norm_B/j
+                if last_term<1e-15:
+                    break
+                beta_factor=beta_factor+gamma_fa(j*(d+2)+2)*last_term
             if m_pass_lowbound == False:
-                tr_first_term = tr_first_term * norm_B / (j + 1)
-                if i * (tr_first_term + lower_bound) > tol:
+                tr_first_term = tr_first_term * (norm_B / (j + 1))
+                if i *tr_first_term + lower_bound > tol:
                     continue
                 else:
                     R_m = residue_norm(j, norm_B, tr_first_term)
                     m_pass_lowbound = True
-            else:
-                tr_first_term = tr_first_term * norm_B / (j + 1)
-                R_m = R_m- tr_first_term
-            if weaker_error(beta_factor,R_m,i)>tol:
-                continue
-            else:
-                total_error=error(norm_B,j,i,d,R_m)
-                if total_error<tol:
-                    no_solution = False
-                    s=i
-                    m=j
-                    break
-        if no_solution == False:
-            break
+            if m_pass_lowbound == True:
+                if weaker_error(beta_factor,R_m,i)>tol:
+                    R_m = R_m - tr_first_term
+                    tr_first_term = tr_first_term * norm_B / (j + 1)
+
+                    continue
+                else:
+                    total_error=error(norm_B,j,i,d,R_m)
+                    R_m = R_m - tr_first_term
+                    tr_first_term = tr_first_term * norm_B / (j + 1)
+                    if total_error<tol:
+                        no_solution = False
+                        s=i
+                        m=j
+                        break
+
     if no_solution==False:
         return s,m
     if no_solution == True:
@@ -135,6 +142,12 @@ def expm_yunwei(A, B,d, tol=None):
             F = F + B
         B = F
     return F,s*m
+def _exact_inf_norm(A):
+    # A compatibility function which should eventually disappear.
+    if sci.sparse.isspmatrix(A):
+        return max(abs(A).sum(axis=1).flat)
+    else:
+        return np.linalg.norm(A, np.inf)
 def overnorm(A):
     if A.dtype==np.complex256:
         return _exact_inf_norm(A)
@@ -276,7 +289,7 @@ for j,_theta in enumerate(theta_m):
     theta_mm.append(a)
 tol = 2**-24
 dim=7
-t=111
+t=11
 H,vec=get_auxiliary(dim,np.float64,1)
 for i in range(500):
     a,x1=expm_yunwei(t*H, vec,9,tol)
